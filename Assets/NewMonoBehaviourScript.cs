709864027d12279cf28c1e56c5029772648675fe
johanagaviria19 <johana.gaviria@campusucc.edu.co>
2026-05-11 18:08:10 -0500

    Actualizacion del proyecto Unity

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{ // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, World!");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
