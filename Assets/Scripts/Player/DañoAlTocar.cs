2701d51319e296fc95334fcd92f03f1daa23e7ad
johanagaviria19 <johana.gaviria@campusucc.edu.co>
2026-05-20 13:33:51 -0500

    feat: actualización sistema de combate, ajustes en niveles y renombrado de Nivel 5

--- Assets/Scripts/Player/DañoAlTocar.cs
+++ Assets/Scripts/Player/DañoAlTocar.cs
@@ -3,39 +3,11 @@ public class DañoAlTocar : MonoBehaviour
 {
     [SerializeField] private int dañoPorToque;
 
-    [SerializeField] private LayerMask capaObjetivo;
-
     void OnTriggerEnter2D(Collider2D collision)
     {
-        ProcesarDaño(collision.gameObject);
-    }
-
-    void OnTriggerStay2D(Collider2D collision)
-    {
-        ProcesarDaño(collision.gameObject);
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        ProcesarDaño(collision.gameObject);
-    }
-
-    void OnCollisionStay2D(Collision2D collision)
-    {
-        ProcesarDaño(collision.gameObject);
-    }
-
-    private void ProcesarDaño(GameObject objeto)
-    {
-        // Solo procesar si el objeto está en la capa objetivo (ej. Player)
-        if (((1 << objeto.layer) & capaObjetivo) != 0)
+        if (collision.TryGetComponent(out VidaPlayer VidaPlayer))
         {
-            VidaPlayer vidaPlayer = objeto.GetComponentInParent<VidaPlayer>();
-            if (vidaPlayer != null)
-            {
-                Debug.Log($"Objeto dañino ({gameObject.name}) tocó al jugador. Daño: {dañoPorToque}");
-                vidaPlayer.TomarDaño(dañoPorToque, transform.position);
-            }
+            VidaPlayer.TomarDaño(dañoPorToque);
         }
     }
 }
